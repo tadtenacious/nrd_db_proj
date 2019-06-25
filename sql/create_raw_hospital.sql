e5670e8c3a0d6198193db79b12aa9404132b0890
Tad Peterson <tadworkswith@gmail.com>
2019-06-24 21:20:45 -0500

    completed script

---
+++ sql/create_raw_hospital.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.raw_hospital (
+    hosp_bedsize bigint,
+    h_contrl bigint,
+    hosp_nrd bigint,
+    hosp_urcat4 bigint,
+    hosp_ur_teach bigint,
+    nrd_stratum bigint,
+    n_disc_u bigint,
+    n_hosp_u bigint,
+    s_disc_u bigint,
+    s_hosp_u bigint,
+    total_disc bigint,
+    year bigint)
