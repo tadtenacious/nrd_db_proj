7d1f2bcc121917d0c44340e60e5f969cc786aa17
Tad Peterson <tadworkswith@gmail.com>
2019-06-29 14:23:34 -0500

    completed mdc names file

--- build_supplemental_files.py
+++ build_supplemental_files.py
@@ -1,7 +1,7 @@
 import os
 import re
 import requests
-import pandas as pd
+from pandas import concat
 from utils.sas import sas_file_to_dataframe
 
 DRG_FORMAT_URL = 'https://www.hcup-us.ahrq.gov/db/tools/DRG_Formats.TXT'
@@ -29,8 +29,8 @@ def main():
         data, 'Value DRGv33f /* DRG - version 33 */', drg_name_cols, '33')
     v34_drg_names = sas_file_to_dataframe(
         data, 'Value DRGv34f /* DRG - version 34 */', drg_name_cols, '34')
-    drg_names = pd.concat([v33_drg_names, v34_drg_names],
-                          ignore_index=True, sort=False)
+    drg_names = concat([v33_drg_names, v34_drg_names],
+                       ignore_index=True, sort=False)
     # clean the data to match 3 digit DRG codes
     drg_names['drg'] = drg_names['drg'].apply(
         lambda x: x if x == '.' else x.zfill(3))
@@ -44,8 +44,20 @@ def main():
         data, 'Value DRG33MSF /* DRG - version 33 to Medical/Surgical Flag */', msf_cols, '33')
     v34_msf = sas_file_to_dataframe(
         data, 'Value DRG34MSF /* DRG - version 34 to Medical/Surgical Flag */', msf_cols, '34')
-    lu_msf = pd.concat([v33_msf, v34_msf], ignore_index=True, sort=False)
+    lu_msf = concat([v33_msf, v34_msf], ignore_index=True, sort=False)
     lu_msf.to_csv('data/lu_drg_MedSurgFlag.csv', index=False, header=False)
+
+    mdc_cols = ['mdc', 'mdc_name']
+    v33_mdc = sas_file_to_dataframe(
+        data, 'Value MDCv33f /* MDC - version 33 */', mdc_cols, '33')
+    v34_mdc = sas_file_to_dataframe(
+        data, 'Value MDCv34f /* MDC - version 34 */', mdc_cols, '34')
+    lu_mdc = concat([v33_mdc, v34_mdc], ignore_index=True, sort=False)
+    # clean the mdcn_name field
+    lu_mdc['mdc_name'] = lu_mdc['mdc_name'].str.replace(
+        r'[ 0-9.]+:', '').str.strip()
+    lu_mdc.to_csv('data/lu_mdc_names.csv', index=False, header=False)
+
     return
 
 
