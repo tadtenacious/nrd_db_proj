c5b95b4a8a2e9afa0cacade1168325be1e86265e
Tad Peterson <tadworkswith@gmail.com>
2019-06-29 12:14:03 -0500

    added get values func and doc strings

--- utils/sas.py
+++ utils/sas.py
@@ -2,9 +2,23 @@ import re
 
 
 def get_section(sas_string, format_name):
+    '''Parse the specific HCUP SAS files that holds the DRG tables.'''
     new_pattern = format_name.replace('*', '\*') + '.*\s+[;]'
     search = re.search(new_pattern, sas_string, re.DOTALL)
     if search:
         return search.string
     else:
         return None
+
+
+def get_values(sas_section):
+    '''Parse SAS section for drg table values'''
+    lines = sas_section.split('\n')
+    values = []
+    for line in lines:
+        if '=' in line:
+            drg, value = line.split('=')
+            vals = (drg.strip().replace("'", ''),
+                    value.strip().replace("'", ''))
+            values .append(vals)
+    return values
