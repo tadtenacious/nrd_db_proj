c83f54fe8bdc50397975c0f0a69743d6d277135e
Tad Peterson <tadworkswith@gmail.com>
2019-06-29 10:33:14 -0500

    added download file function

--- build_supplemental_files.py
+++ build_supplemental_files.py
@@ -2,3 +2,26 @@ import os
 import re
 import requests
 import pandas as pd
+
+DRG_FORMAT_URL = 'https://www.hcup-us.ahrq.gov/db/tools/DRG_Formats.TXT'
+DRG_FILE_NAME = 'DRG_Formats.TXT'
+SAVE_PATH = os.path.join('data', DRG_FILE_NAME)
+
+
+def download_file(url, save_path):
+    with open(save_path, 'w') as f:
+        resp = requests.get(url)
+        f.write(resp.content)
+    return
+
+
+def main():
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not os.path.exists(SAVE_PATH):
+        download_file(DRG_FORMAT_URL, SAVE_PATH)
+    return
+
+
+if __name__ == "__main__":
+    main()
