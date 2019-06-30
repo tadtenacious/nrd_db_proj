795f9c561ad32763a43f5f8d27ba4a9b5a76168c
Tad Peterson <tadworkswith@gmail.com>
2019-06-30 12:11:04 -0500

    completed drg names table script

---
+++ sql/create_lu_drg_names.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS public.lu_drg_names;
+
+CREATE TABLE public.lu_drg_names (
+    drgver character varying (255) COLLATE pg_catalog. "default",
+    drg character varying (255) COLLATE pg_catalog. "default",
+    "drgName" character varying (255) COLLATE pg_catalog. "default")
