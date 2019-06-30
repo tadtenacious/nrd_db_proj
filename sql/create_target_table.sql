f493f483bc9f35f8976978c3725cf3d747a3ff8d
Tad Peterson <tadworkswith@gmail.com>
2019-06-30 12:21:29 -0500

    completed target table script

---
+++ sql/create_target_table.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS public.target_table;
+
+CREATE TABLE public.target_table AS SELECT DISTINCT
+    a.key_nrd AS key_nrd,
+    CASE WHEN b.key_nrd IS NULL THEN
+        0
+    ELSE
+        1
+    END target --Target column for machine learning model
+FROM
+    raw_core a
+    LEFT JOIN readmit_core b ON a.key_nrd = b.key_nrd;
+
