6e4c84cfba40e144464e854c49f6e92564452304
Tad Peterson <tadworkswith@gmail.com>
2019-06-30 14:38:40 -0500

    abbreviated and remove caps in table name

--- sql/create_lu_drg_MedSurgFlag.sql
+++ sql/create_lu_drg_MedSurgFlag.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS public. "lu_drg_MedSurgFlag";
+DROP TABLE IF EXISTS public.lu_drg_msf;
 
-CREATE TABLE public. "lu_drg_MedSurgFlag" (
+CREATE TABLE public.lu_drg_msf (
     drgver character varying (255) COLLATE pg_catalog. "default",
     drg character varying (255) COLLATE pg_catalog. "default",
     msf character varying (255) COLLATE pg_catalog. "default")
