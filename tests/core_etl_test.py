8be06df3be6f539a550cb34e0a062203ee8d05ac
Tad Peterson <tadworkswith@gmail.com>
2019-06-30 12:44:35 -0500

    added sql tests and reformatted

--- tests/core_etl_test.py
+++ tests/core_etl_test.py
@@ -15,10 +15,12 @@ def make_sample(in_path, new_path):
 
 def main():
     from utils.db import build_connection, check_table, reader, load_csv
-    print('Reading sql files...')
     core = reader('../sql/create_raw_core.sql')
     hosp = reader('../sql/create_raw_hospital.sql')
     severity = reader('../sql/create_raw_severity.sql')
+    # tables created entirely in SQL
+    readmit = reader('../sql/create_readmit_core.sql')
+    target = reader('../sql/create_target_table.sql')
     tables = {
         'raw_core': core,
         'raw_hospital': hosp,
@@ -42,35 +44,80 @@ def main():
             print('Please put the HCUP CSV files in the data directory')
             print(e)
     print('Connecting to server...')
+    print('Running Tests.')
+    total_tests = 0
+    failed_tests = []
+
     try:
         con = build_connection('../config.json')
-        print('Connection to server successful!')
         cursor = con.cursor()
     except psycopg2.OperationalError as e:
         print('Connection to server unsuccesful.\nRerun setp_config.py')
         print(e)
+        print('\nCannot run tests without successful connection to server.')
+        return
+    drop_tables = []
     for table, statement in tables.items():
-        print('Creating {}'.format(table))
         try:
             cursor.execute(statement)
             con.commit()
             check = check_table(cursor, table)
             if check:
-                print('Successfully created {}'.format(table))
                 file_to_load = csv_to_table[table]
-                print('loading {}'.format(table))
                 load_csv(cursor, file_to_load, table)
+                total_tests += 1
                 con.commit()
             else:
+                failed_tests.append('Loading {}'.format(table))
                 print('Failed creating {}'.format())
+                total_tests += 1
         except psycopg2.OperationalError as e:
             print('Failed creating {}'.format(table))
-        print('Dropping {}'.format(table))
-        cursor.execute('DROP TABLE IF EXISTS {}'.format(table))
+            failed_tests.append('Create {}'.format(table))
+        drop_tables.append(table)
+        total_tests += 1
+    try:
+        cursor.execute(readmit)
+        con.commit()
+        check_readmit = check_table(cursor, 'readmit_core')
+        if not check_readmit:
+            failed_tests.append('Create readmit_core')
+    except psycopg2.OperationalError as e:
+        failed_tests.append('Create readmit_core')
+        print('Failed creating readmit_core')
+        print(e)
+    total_tests += 1
+    drop_tables.append('readmit_core')
+    try:
+        cursor.execute(target)
+        con.commit()
+        check_target = check_table(cursor, 'target_table')
+        if not check_target:
+            failed_tests.append('Create target_table')
+    except psycopg2.OperationalError as e:
+        print('Failed creating target_table')
+        print(e)
+        failed_tests.append('Create target_table')
+    total_tests += 1
+    drop_tables.append('target_table')
+    print('Dropping tables.')
+    for table in drop_tables:
+        try:
+            cursor.execute('DROP TABLE IF EXISTS {}'.format(table))
+        except psycopg2.OperationalError as e:
+            print(e)
     con.close()
     print('Removing Sample Files')
     for sample_file in csv_files.values():
         os.remove(sample_file)
+    print('>>>Summary<<<')
+    num_failed = len(failed_tests)
+    print('\tTotal tests run: {}'.format(total_tests))
+    print('\tSuccessful tests: {}'.format(total_tests - num_failed))
+    print('\tFailed tests: {}'.format(num_failed))
+    if num_failed > 0:
+        for test in failed_tests:
+            print('\t' + test)
 
 
 if __name__ == "__main__":
