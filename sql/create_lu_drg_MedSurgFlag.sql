de2f08cc90b532b19421ed5d8795b43b2dcf6cb4
Tad Peterson <tadworkswith@gmail.com>
2019-06-30 12:09:44 -0500

    completed medsurg flag table script

---
+++ sql/create_lu_drg_MedSurgFlag.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS public. "lu_drg_MedSurgFlag";
+
+CREATE TABLE public. "lu_drg_MedSurgFlag" (
+    drgver character varying (255) COLLATE pg_catalog. "default",
+    drg character varying (255) COLLATE pg_catalog. "default",
+    msf character varying (255) COLLATE pg_catalog. "default")
