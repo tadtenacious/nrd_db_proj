c677b1c46c39af5dcf4b12291fbcc4d9de8e88b3
Tad Peterson <tadworkswith@gmail.com>
2019-07-08 19:13:16 -0500

    added argument for connection builder

--- etl.py
+++ etl.py
@@ -10,7 +10,7 @@ def main():
     # create the supplemental files
     build_supplemental_files()
     try:
-        con = build_connection()
+        con = build_connection('config.json')
         cursor = con.cursor()
     except psycopg2.OperationalError as e:
         print('An error occured connecting to the server. Please check your configuration file.')
