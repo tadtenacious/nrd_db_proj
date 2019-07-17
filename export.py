23a5d5784b4b9c18bb7a79724fa418a465676a3a
Tad Peterson <tadworkswith@gmail.com>
2019-07-16 20:45:03 -0500

    completed script

---
+++ export.py
@@ -0,0 +1,38 @@
+from utils.db import build_connection
+import argparse
+
+
+def export(sample=False):
+    con = build_connection('config.json')
+    cursor = con.cursor()
+    select = 'SELECT * FROM feature_set'
+    table_sample = ' TABLESAMPLE SYSTEM (1) REPEATABLE (101)'
+    if sample:
+        query = select + table_sample
+        file_name = 'data/feature_set_sample.csv'
+    else:
+        query = select
+        file_name = 'data/feature_set.csv'
+    output = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(query)
+    if sample:
+        msg = 'Exporting 1% sample.'
+    else:
+        msg = 'Exporting full dataset. This may take a while.'
+    print(msg)
+    with open(file_name, 'w') as f:
+        cursor.copy_expert(output, f)
+    con.close()
+    print('Data export complete.')
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Export data from postgresql server')
+    parser.add_argument(
+        '--sample', help='Option to export 1% sample', action='store_true')
+    args = parser.parse_args()
+    sample = False
+    if args.sample:
+        sample = True
+    export(sample)
