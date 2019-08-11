1012110a38b2a96ddb1589d53e6b89925070a1c1
Tad Peterson <tadworkswith@gmail.com>
2019-08-10 21:25:45 -0500

    changed table name in test_load_csv

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -32,9 +32,9 @@ def test_load_csv(cursor, tmpdir):
     p = tmpdir.join('a_csv_file.csv')
     p.write('1,1\n2,2')
     cursor.execute(
-        """CREATE TABLE IF NOT EXISTS test.test_table (col1 INT, col2 INT)""")
+        """CREATE TABLE IF NOT EXISTS test.test_load (col1 INT, col2 INT)""")
     csv_file = os.path.join(p.dirname, p.basename)
-    load_csv(cursor, csv_file, 'test.test_table')
-    cursor.execute('SELECT * FROM test.test_table')
+    load_csv(cursor, csv_file, 'test.test_load')
+    cursor.execute('SELECT * FROM test.test_load')
     check_data = [tuple(i) for i in cursor.fetchall()]
     assert check_data == [(1, 1), (2, 2)]
