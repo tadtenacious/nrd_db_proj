048cd313867727d6d72e52a9c5ad8351f3aeff98
Tad Peterson <tadworkswith@gmail.com>
2019-08-10 21:15:00 -0500

    finished unit tests for src.db

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,6 +1,9 @@
 import os
 import sys
-import pytest
+# import pytest
+
+sys.path.append('..')
+from src.db import build_connection, reader, check_table, load_csv
 
 
 def test_config_file_exists():
@@ -8,3 +11,30 @@ def test_config_file_exists():
     exists = os.path.exists(config_path)
     is_file = os.path.isfile(config_path)
     assert exists and is_file
+
+
+def test_reader(tmpdir):
+    p = tmpdir.join('a_file.txt')
+    p.write('This is a test')
+    filename = os.path.join(p.dirname, p.basename)
+    content = reader(filename)
+    assert content == 'This is a test'
+
+
+def test_check_table(cursor):
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS test.test_table (col1 INT, col2 INT)""")
+    table_exists = check_table(cursor, 'test_table', 'test')
+    assert table_exists
+
+
+def test_load_csv(cursor, tmpdir):
+    p = tmpdir.join('a_csv_file.csv')
+    p.write('1,1\n2,2')
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS test.test_table (col1 INT, col2 INT)""")
+    csv_file = os.path.join(p.dirname, p.basename)
+    load_csv(cursor, csv_file, 'test.test_table')
+    cursor.execute('SELECT * FROM test.test_table')
+    check_data = [tuple(i) for i in cursor.fetchall()]
+    assert check_data == [(1, 1), (2, 2)]
