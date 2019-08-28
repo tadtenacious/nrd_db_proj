01e1b967c2ef9e37cf43b9629b3f8a3c8d415250
Tad Peterson <tadworkswith@gmail.com>
2019-08-28 15:14:07 -0500

    consolidated main etl

--- src/etl.py
+++ src/etl.py
@@ -1,6 +1,8 @@
 import os
 import psycopg2
-from .db import check_table, reader, load_csv
+from .db import check_table, reader, load_csv, build_connection
+from src.supplement import build_supplemental_files
+from src.views import make_views
 
 
 def etl(con, cursor):
@@ -103,4 +105,20 @@ def etl(con, cursor):
         print(e)
         con.rollback()
     print('Initial ETL completed.')
+    return
+
+
+def do_etl():
+    # create the supplemental files
+    build_supplemental_files()
+    try:
+        con = build_connection('config.json')
+        cursor = con.cursor()
+    except psycopg2.OperationalError as e:
+        print('An error occured connecting to the server. Please check your configuration file.')
+        print(e)
+        return
+    etl(con, cursor)
+    make_views(con, cursor)
+    con.close()
     return
