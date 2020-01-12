3715ec0dd2403d0ba70c625b39afc442d05eed51
Tad Peterson <tadworkswith@gmail.com>
2020-01-11 18:56:13 -0600

    tested new preprocessing steps

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,13 +1,14 @@
 import os
 import sys
 from pandas import DataFrame, Series
-from numpy import nan
+from pandas.testing import assert_frame_equal
+import numpy as np
 import requests
-# import pytest
+import pytest
 
 sys.path.append('..')
 from src.db import build_connection, reader, check_table, load_csv
-from src.preprocessing import fill_mean, fill_cat, preprocess
+from src.preprocessing import add_age_bins, prepare_chunk, handle_missing, process_chunk
 from src.supplement import DRG_FORMAT_URL, download_file
 
 
@@ -54,34 +55,6 @@ def test_load_csv(cursor, tmpdir):
     assert check_data == [(1, 1), (2, 2)]
 
 
-def test_fill_cat():
-    df = DataFrame(
-        {'col1': [1, 2, 3, -5], 'col2': [-4, -3, -2, 0]}).pipe(fill_cat)
-    assert df.equals(
-        DataFrame({'col1': [1.0, 2.0, 3.0, -1.0], 'col2': [-4, -3, -2, 0]}))
-
-
-def test_fill_mean():
-    # fill with means from dataframe
-    df1 = DataFrame(
-        {'col1': [-99, 10, 10], 'col2': [-98, 100, 100]}).pipe(fill_mean)
-    assert df1.equals(DataFrame(
-        {'col1': [10.0, 10.0, 10.0], 'col2': [-98, 100, 100]}))
-    # fill with means from another source
-    means = Series([9, 9], index=['col1', 'col2'])
-    df2 = DataFrame(
-        {'col1': [-99, 10, 10], 'col2': [-98, 100, 100]}).pipe(fill_mean, means=means)
-    assert df2.equals(DataFrame(
-        {'col1': [9.0, 10.0, 10.0], 'col2': [-98, 100, 100]}))
-
-
-def test_preprocessing():
-    df1 = DataFrame(
-        {'col1': [100, 100, -99], 'col2': [1, -4, -5]}).pipe(preprocess)
-    assert df1.equals(
-        DataFrame({'col1': [100.0, 100.0, 100.0], 'col2': [1.0, -4.0, -1.0]}))
-
-
 def test_download_file(tmpdir):
     # test downloading the file
     p = tmpdir.join('the_sas_file.txt')
@@ -96,3 +69,97 @@ def test_download_file(tmpdir):
     assert 'Value DRGv34f /* DRG - version 34 */' in content
     assert 'Value DRG33MSF /* DRG - version 33 to Medical/Surgical Flag */' in content
     assert 'Value DRG34MSF /* DRG - version 34 to Medical/Surgical Flag */' in content
+
+
+@pytest.fixture
+def chunk():
+    df = DataFrame({
+        'age': [0, 4, 5, 6, 19, 37, 55, 73, 72, 54],
+        'categorical_column': ['-6', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        'continuous_column': ['10', '20', '30', '40', '50', '60', '70', '80', '90', '-100'],
+        'hosp_nrd': list('0123456789'),
+        'key_nrd': list('9874123056')
+    })
+    return df
+
+
+@pytest.fixture
+def dtypes():
+    data_types = {
+        'age': 'int8',
+        'categorical_column': 'int8',
+        'continuous_column': 'int64',
+    }
+    return data_types
+
+
+def test_prepare_chunk(chunk, dtypes):
+    new_chunk = prepare_chunk(chunk, dtypes)
+    assert 'key_nrd' not in new_chunk.columns
+    assert 'hosp_nrd' not in new_chunk.columns
+    assert new_chunk['age'].dtype == 'int8'
+    assert new_chunk['categorical_column'].dtype == 'int8'
+    assert new_chunk['continuous_column'].dtype == 'int64'
+
+
+def test_add_age_bins(chunk, dtypes):
+    new_chunk = chunk = chunk.pipe(
+        prepare_chunk, dtypes=dtypes).pipe(add_age_bins)
+    expected_columns = ['age', 'categorical_column', 'continuous_column',
+                        '5-18', '19-36', '37-54', '55-72', '73+']
+    assert new_chunk.columns.tolist() == expected_columns
+    excpected_dtype_lst = ['int8', 'int8', 'int64',
+                           'uint8', 'uint8', 'uint8', 'uint8', 'uint8']
+    expected_dtypes = {k: v for k, v in zip(
+        expected_columns, excpected_dtype_lst)}
+
+    expected_data = [[0, -6, 10, 0, 0, 0, 0, 0],
+                     [4, 1, 20, 1, 0, 0, 0, 0],
+                     [5, 2, 30, 1, 0, 0, 0, 0],
+                     [6, 3, 40, 1, 0, 0, 0, 0],
+                     [19, 4, 50, 0, 1, 0, 0, 0],
+                     [37, 5, 60, 0, 0, 1, 0, 0],
+                     [55, 6, 70, 0, 0, 0, 1, 0],
+                     [73, 7, 80, 0, 0, 0, 0, 1],
+                     [72, 8, 90, 0, 0, 0, 1, 0],
+                     [54, 9, -100, 0, 0, 1, 0, 0]]
+    expected_chunk = DataFrame(
+        data=expected_data, columns=expected_columns).astype(expected_dtypes)
+    assert_frame_equal(expected_chunk, new_chunk)
+
+
+def test_handle_missing(chunk, dtypes):
+    new_chunk = chunk.pipe(prepare_chunk, dtypes=dtypes).pipe(handle_missing)
+    expected_chunk = DataFrame({
+        'age': [0, 4, 5, 6, 19, 37, 55, 73, 72, 54],
+        'categorical_column': [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        'continuous_column': [10, 20, 30, 40, 50, 60, 70, 80, 90, np.nan],
+    }).astype({'age': 'int8', 'categorical_column': 'int8', 'continuous_column': 'float64'})
+    assert new_chunk['age'].dtype == 'int8'
+    assert new_chunk['categorical_column'].dtype == 'int8'
+    assert new_chunk['continuous_column'].dtype == 'float64'
+    assert_frame_equal(expected_chunk, new_chunk)
+
+
+def test_process_chunk(chunk, dtypes):
+    processed_chunk = process_chunk(chunk, dtypes)
+    expected_columns = ['age', 'categorical_column', 'continuous_column', '5-18', '19-36',
+                        '37-54', '55-72', '73+']
+    assert processed_chunk.columns.tolist() == expected_columns
+    expected_data = [[0., -1., 10., 0., 0., 0., 0., 0.],
+                     [4., 1., 20., 1., 0., 0., 0., 0.],
+                     [5., 2., 30., 1., 0., 0., 0., 0.],
+                     [6., 3., 40., 1., 0., 0., 0., 0.],
+                     [19., 4., 50., 0., 1., 0., 0., 0.],
+                     [37., 5., 60., 0., 0., 1., 0., 0.],
+                     [55., 6., 70., 0., 0., 0., 1., 0.],
+                     [73., 7., 80., 0., 0., 0., 0., 1.],
+                     [72., 8., 90., 0., 0., 0., 1., 0.],
+                     [54., 9., np.nan, 0., 0., 1., 0., 0.]]
+    expected_dtype_lst = ['int8', 'int8', 'float64',
+                          'uint8', 'uint8', 'uint8', 'uint8', 'uint8']
+    expected_dtypes = {k: v for k, v in zip(
+        expected_columns, expected_dtype_lst)}
+    expected_chunk = DataFrame(
+        data=expected_data, columns=expected_columns).astype(expected_dtypes)
+    assert_frame_equal(expected_chunk, processed_chunk)
