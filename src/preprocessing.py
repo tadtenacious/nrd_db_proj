e263395d2d64e476b4832e6b90ea8bad4a979f4c
Tad Peterson <tadworkswith@gmail.com>
2020-01-11 18:56:02 -0600

    new preprocessing steps

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,21 +1,47 @@
 from numpy import nan
+from pandas import cut, get_dummies
+import json
 
 
-def fill_mean(dataframe, means=None):
-    '''Set missing values for continous variables, values less than -98
-    and fill with the mean.'''
-    dataframe[dataframe < -98] = nan
-    if means is None:
-        means = dataframe.mean()
-    return dataframe.fillna(means)
+def prepare_chunk(chunk, dtypes):
+    '''Prepare the dataframe chunk by converting the datatypes and dropping the
+    unique identifiers, key_nrd and hosp_nrd.
+    '''
+    chunk = chunk.astype(dtypes).drop(['key_nrd', 'hosp_nrd'], axis=1)
+    chunk
+    return chunk
 
 
-def fill_cat(dataframe):
-    '''Set missing values for categorical variables and fill with -1.'''
-    dataframe[dataframe < -4] = nan
-    return dataframe.fillna(-1)
+def handle_missing(chunk):
+    '''A function to handle or set missing values. Categorical variables that
+     are missing have values between -98 and -5. These are all set to -1. As
+     they are all the same values, they do not need to be set during the CV
+     split. Continuous variables have missing values that are less than or 
+     equal to -99. This will be set to nan and be filled during the CV split
+     to avoid data leakage.
+    '''
+    categorical_missing = chunk.ge(-98) & chunk.le(-5)
+    chunk = chunk.mask(categorical_missing, -1)
+    nulls = chunk.le(-99)
+    chunk = chunk.mask(nulls, nan)
+    return chunk
 
 
-def preprocess(dataframe):
-    '''Calls fill_mean and fill_cat to preprocess the hcup data'''
-    return dataframe.pipe(fill_mean).pipe(fill_cat)
+def add_age_bins(chunk):
+    '''Add the age bin features but binning the ages and the one hot encoding 
+    them.
+    '''
+    age_labels = ['0-3', '5-18', '19-36', '37-54', '55-72', '73+']
+    age_bins = [0, 4, 19, 37, 55, 73, 90]
+    chunk = chunk.assign(age_bins=cut(chunk['age'], age_bins, right=False,
+                                      labels=age_labels))
+    dummies = get_dummies(chunk['age_bins'], drop_first=True)
+    chunk = chunk.join(dummies).drop('age_bins', axis=1)
+    return chunk
+
+
+def process_chunk(chunk, dtypes):
+    '''A function to run all initial preprocessing steps.'''
+    chunk = chunk.pipe(prepare_chunk, dtypes=dtypes).pipe(add_age_bins)\
+        .pipe(handle_missing)
+    return chunk
