10ea6b7c95c00495e9a426b035737c97584a1319
Tad Peterson <tadworkswith@gmail.com>
2020-01-16 19:17:07 -0600

    changed to use named cursor, parquet, dask

--- src/export.py
+++ src/export.py
@@ -1,70 +1,73 @@
 import json
 import os
-import numpy as np
-from pandas import read_sql, Series
-from .preprocessing import process_chunk
-from dask import delayed
+import shutil
+from numpy import nan
+from pandas import DataFrame, Series, read_sql
 import dask.dataframe as dd
-from multiprocessing import cpu_count, Pool
 
 
-def write_chunk(chunk, dtypes, file_name):
-    # chunk.pipe(process_chunk, dtypes=dtypes).to_parquet(
-    #     file_name, engine='fastparquet', compression='snappy')
-    chunk.pipe(process_chunk, dtypes=dtypes).to_csv(file_name, index=False)
-    return
+def iter_many(cursor, columns, size=50000, dtypes=None):
+    '''A generator to iterate over rows of a psycopg2 cursor. Psycopg2 or 
+    PostgreSQL require a named cursor in order to have a server side cursor. 
+    Otherwise the entire query result set is loaded into memory. 
+
+    Parameters
+    ----------
+    cursor: a cursor from the psycopg2 connection object that is named.
+    columns: a list columns from query required for manual dataframe constructor.
+    size: integer of how many rows to return per iteration, default 50,000.
+    dtypes: a dictionary of columns to pandsa datatypes, default None.
+    '''
+    while True:
+        records = cursor.fetchmany(size=size)
+        if not records:
+            break
+        if dtypes:
+            df = DataFrame(data=records, columns=columns).astype(dtypes)
+        else:
+            df = DataFrame(data=records, columns=columns)
+        yield df
 
 
 def export(connection, sample=False):
     '''A function to export the feature set from the database. 
-    Requires the cursor and bloolean value for sample. sample=True exports 1% sample.'''
-    select = 'SELECT * FROM feature_set'
-    table_sample = " WHERE substring(CAST(key_nrd AS VARCHAR(20)),8,1) = '1' and substring(CAST(key_nrd AS VARCHAR(20)),9,1)='3'"
+    Requires the connection and bloolean value for sample. sample=True exports 1% sample.'''
     if sample:
-        query = select + table_sample
-        file_name = 'data/sample_data.parquet'
-    else:
-        query = select
-        file_name = 'data/full_data.parquet'
-    if sample:
+        query = 'SELECT * FROM feature_set_sample'
+        file_name = 'data/sample_data'
         msg = 'Exporting 1% sample.'
+        cursor_name = 'fetchsample'
     else:
+        query = 'SELECT * FROM feature_set'
+        file_name = 'data/full_data'
         msg = 'Exporting full dataset. This may take a while.'
+        cursor_name = 'fetchfull'
     print(msg)
+
     with open('data/dtypes.json', 'r') as f:
         dtypes = json.loads(f.read())
-    if sample:
-        chunks = read_sql(query, connection, chunksize=20000)
-        frames = [delayed(process_chunk)(chunk=chunk, dtypes=dtypes)
-                  for chunk in chunks]
-        ddf = dd.from_delayed(frames)
-        ddf.to_parquet(file_name, engine='fastparquet',
-                       compression='snappy', write_index=False)
-    else:
+
+    feature_cols = list(dtypes.keys())
+
+    if not os.path.exists('temp'):
         os.mkdir('temp')
-        nprocs = cpu_count() - 1
-        pool = Pool(nprocs)
-        chunks = read_sql(query, connection, chunksize=20000)
-        out = [pool.apply_async(write_chunk, args=(
-            chunk, dtypes, f'temp/part{i}.csv'),) for i, chunk in enumerate(chunks, 1)]
-        pool.close()
-        # ddf = dd.read_sql_table('feature_set', connection, index_col='key_nrd')
-        # ddf = ddf.astype(dtypes).drop('hosp_nrd', axis=1)
-        # age_labels = ['0-3', '5-18', '19-36', '37-54', '55-72', '73+']
-        # age_bins = [-1, 4, 19, 37, 55, 73, 100]
-        # ddf['age_bins'] = ddf['age'].map_partitions(
-        #     cut, bins=age_bins, labels=age_labels)
-        # for i in age_labels[1:]:
-        #     ddf[i] = ddf['age_bins'].apply(lambda x, val=i: 1 if
-        #                                    x == val else 0, meta=Series(dtype='int8', name=i))
-        # ddf = ddf.drop('age_bins', axis=1)
-        # cat_replace = ddf.gt(-99).lt(-4)
-        # ddf = ddf.mask(cat_replace, -1)
-        # nulls = ddf.lt(-98)
 
-        # ddf = ddf.mask(nulls, np.NaN)
+    cursor = connection.cursor(name=cursor_name)  # creates server side cursor
+    cursor.execute(query)
+    chunks = iter_many(cursor, columns=feature_cols,
+                       size=100000, dtypes=dtypes)
+    for i, chunk in enumerate(chunks, 1):
+        tpath = os.path.join('temp', f'prefeature{i}.parquet')
+        chunk.to_parquet(tpath, index=False, engine='pyarrow',
+                         compression='snappy')
 
-        # ddf.to_parquet(file_name, engine='fastparquet',
-        #                compression='snappy', write_index=False)
+    df = dd.read_parquet('temp', engine='pyarrow')
+    cat_replace = df.gt(-99) & df.lt(-4)
+    df = df.mask(cat_replace, -1)
+    nulls = df.lt(-98)
+    df = df.mask(nulls, nan)
+    df.to_parquet(file_name, engine='pyarrow',
+                  compression='snappy', write_index=False)
+    shutil.rmtree('temp')
     print('Data export complete.')
     return
